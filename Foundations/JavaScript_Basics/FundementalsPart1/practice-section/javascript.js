eac3f9014ad2102cb6b11849fec5a243aaac30f5
boostaboosh <mail.laurencesnow@gmail.com>
2022-06-07 18:56:21 +0100

    Complete JavaScript Fundamentals Part1 practice

---
+++ Foundations/JavaScript_Basics/FundementalsPart1/practice-section/javascript.js
@@ -0,0 +1,24 @@
+console.log(23 + 97); //ex1
+console.log(1 + 33 + 753 + 3 + 3 + 7); //ex2
+console.log((4 + 6 + 9) / 77); //ex3
+
+//ex4
+let a = 10;
+console.log(a);
+console.log(9 * a);
+let b = 7 * a;
+console.log(b);
+
+//ex5
+const max = 57;
+let actual = max - 13;
+let percentage = actual / max;
+
+//ex6
+const numberOfDaysInAWeek = 7;
+let today = "tuesday";
+console.log(+today);
+let someSpaces = "          ";
+console.log(someSpaces);
+console.log(+someSpaces);
+console.log(Number(someSpaces));
