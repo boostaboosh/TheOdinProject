a65e2d2371415e5d24fd1f956ff81b996abeb154
boostaboosh <mail.laurencesnow@gmail.com>
2022-11-11 09:03:23 +0000

    Attaching event listeners to groups of nodes
    This commit adds the exercises from The Odin Project DOM manipulation
    and events tutorial that have to do with attaching listeners to multiple
    document object model element nodes without have to repeat lines of JS
    code.
    
    This commit also adds the e, i.e., event, parameter to a function call.
    This is an event object which references the event itself that triggered
    the listener and subsequent function call.

--- Foundations/JavaScript_Basics/DOMManipulationAndEvents/EventsAndDOMManipulationExercises/script.js
+++ Foundations/JavaScript_Basics/DOMManipulationAndEvents/EventsAndDOMManipulationExercises/script.js
@@ -6,4 +6,21 @@ btn.onclick = () => alert('Hello World');
 const btnTwo = document.querySelector('#btnTwo');
 btnTwo.addEventListener('click', () => {
     alert('Hello World');
-})
+});
+
+// passing object that references the event itself as an argument to the function call
+const btnThree = document.querySelector('#btnThree');
+btnThree.addEventListener('click', (e) => {
+    console.log(e);
+    console.log(e.target);
+    e.target.style.background = 'blue';
+});
+
+// adding an event listener to multiple buttons with one command
+const buttons = document.querySelectorAll('button');
+for (let button of buttons) {
+    button.addEventListener('click', (event) => {
+        alert(button.id);
+        event.target.style.color = 'red';
+    })    
+}
